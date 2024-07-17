efa731dbc5102fd76fa355bf770d4dc31617373b
peire <peirettimatteo1@gmail.com>
2024-07-17 21:53:34 +0200

    add hide template

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -20,18 +20,21 @@ export class CalendarComponent implements OnInit  {
   daysOfMonth$!: Observable<DayMonth[]>;
   selectedDate: Date | null;
   homeform!: FormGroup;
-  hours: number[] = [1, 2, 3, 4, 5, 6, 7, 8]; 
+  hours: number[] = [0,1, 2, 3, 4, 5, 6, 7, 8]; 
   places: string[] = ['SEDE','SMART_WORKING','SEDE_CLIENTE'];
   totalHoursWorked: number = 0;
   totalIllnessHours: number = 0;
   totalHoliday: number = 0;
   totalDayOff: number = 0;
   totalMonthHour: number=0;
+  viewTotal : boolean = false;
 
   constructor(private apiService: ApiService) {
     this.selectedDate = null;
   }
 
+
+ 
  
 
   ngOnInit(): void {
@@ -44,6 +47,9 @@ export class CalendarComponent implements OnInit  {
     this.homeform.valueChanges.subscribe(() => {
       this.calculateTotals();
     });
+
+
+     
   }
 
   getCalendar(month: string, year: string) {
@@ -77,7 +83,7 @@ export class CalendarComponent implements OnInit  {
       }));
     });
 
-    this.calculateTotals();
+    this.calculateTotals(); // richiamo il metodo che mi fa la somma delle ore
   }
 
   dateSelected(event: MatDatepickerInputEvent<Date>) {
@@ -100,18 +106,20 @@ export class CalendarComponent implements OnInit  {
     this.totalHoliday = 0;
     this.totalDayOff = 0;
   
-    // Recupero il FormArray 'days' dal FormGroup
+    // Recupero il FormArray 'days' dal FormGroup dichiarato in cima
     const daysFormArray = this.homeform.get('days') as FormArray;
   
     // Itero sui controlli del form per sommare i valori
     daysFormArray.controls.forEach((control) => {
-      const group = control as FormGroup;
+      const group = control as FormGroup; //dall'array estraggo il formgroup che contiene i controlli
       this.totalHoursWorked += group.get('hoursWorked')?.value || 0;
       this.totalIllnessHours += group.get('illnessHours')?.value || 0;
       this.totalHoliday += group.get('holiday')?.value || 0;
       this.totalDayOff += group.get('dayOff')?.value || 0;
     });
     this.totalMonthHour= this.totalHoursWorked+this.totalIllnessHours+this.totalHoliday+ this.totalDayOff
+    this.viewTotal = this.totalMonthHour > 0; // Imposta viewTotal a true se ci sono ore totali maggiori di zero
+
   }
   
 
@@ -122,7 +130,6 @@ export class CalendarComponent implements OnInit  {
       // Utilizzo getRawValue() per ottenere tutti i dati del form, inclusi i campi disabilitati
       const formData = daysFormArray.getRawValue();
       console.log(formData);
-      // Processa i dati del form come necessario
-    }
+     }
   }
 }
